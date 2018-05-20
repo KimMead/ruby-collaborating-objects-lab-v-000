e9bc28bb6aea37af8a2a588b6d67bf0ab6c47343
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-05-20 00:45:56 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -8,14 +8,14 @@ class MP3Importer
   def files
     files = []
     Dir.new(self.path).each do |file|
-      files << file 
+      files << file
     end
     files
   end
 
   def import
-    imported_files = self.files
-    imported_files.each do |file|
-      new_song = Song.new_by_filename(file)
+    self.files.each do |filename|
+    Song.new_by_filename(filename)
   end
+end
 end
